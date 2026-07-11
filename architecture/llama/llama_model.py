eddbf3f461ee7b93b3d097ac5a365ad4d3fc6fd8
Hedi Bouchehda <h.bouchehdaa@gmail.com>
2026-07-12 00:06:11 +0200

    llama architecture

---
+++ architecture/llama/llama_model.py
@@ -0,0 +1,34 @@
+import torch 
+import torch.nn as nn 
+from architecture.llama.llama_transformer_block import LlamaTransformerBlock
+from architecture.normalization.RMSNorm import RMSNorm
+
+## configuration inputs
+# "d_model" 
+# "num_heads" 
+# "num_kv_groups" 
+# "context_window"
+# vocab_size 
+# n layers
+
+class LlamaModel(nn.Module) : 
+    def __init__(self,cfg) : 
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"],cfg["d_model"])
+
+        self.llama_transformer_blocks = nn.Sequential(
+            *[LlamaTransformerBlock(cfg) for _ in range(cfg["n_layers"])]
+        )
+
+        self.final_norm = RMSNorm(cfg["d_model"])
+        self.out_head = nn.Linear(cfg["d_model"],cfg["vocab_size"])
+    def forward(self,input_tokens) : 
+        x = self.tok_emb(input_tokens)
+        x = self.llama_transformer_blocks(x)
+        x = self.final_norm(x) 
+        logits = self.out_head(x)
+        return logits
+
+
+
+    
