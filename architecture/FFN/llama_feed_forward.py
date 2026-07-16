3f21ca6611e8e21882694f3702ede5a4e71e0cde
Hedi Bouchehda <h.bouchehdaa@gmail.com>
2026-07-16 22:14:16 +0200

    updating ffn dim compute method in llama feed forward  + finished the training loop

--- architecture/FFN/llama_feed_forward.py
+++ architecture/FFN/llama_feed_forward.py
@@ -1,11 +1,11 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-def ffn_dim(d_model, multiple_of=64):
+def ffn_dim(d_model, multiple_of=64, ffn_dim_multiplier=None):
     h = int(8 * d_model / 3)
+    if ffn_dim_multiplier is not None:
+        h = int(ffn_dim_multiplier * h)
     return multiple_of * ((h + multiple_of - 1) // multiple_of)
-
 
 class SwiGLUMLP(nn.Module):
     def __init__(self, d_model, multiple_of=64):
--- training/train.py
+++ training/train.py
@@ -4,9 +4,19 @@ from training.data_loader import DataLoaderHF
 from typing import Dict, List, Tuple
 
 class TrainingLoop :  
-    def __init__(self,model:LlamaModel,model_config:Dict,data_loader:DataLoaderHF,data_set:List[str],device,dtype=torch.bfloat16,
-                 batch_size:int=5, lr:float=3e-4, weight_decay:float=0.1,adamw_betas:Tuple=(0.9,0.95), warmup_steps:int=1000, 
-                 warmup_step_percentage=0.05 ) :
+    def __init__(self,model:LlamaModel,model_config:Dict,
+                 data_loader:DataLoaderHF,
+                 data_set:List[str],
+                 device = torch.device("cuda" if torch.cuda.is_available() else "cpu") , 
+                 dtype=torch.bfloat16,
+                 batch_size:int=5, lr:float=3e-4,
+                 weight_decay:float=0.1,
+                 adamw_betas:Tuple=(0.9,0.95),
+                 warmup_steps:int=1000, 
+                 warmup_step_percentage:float=0.02, 
+                 decay_step_percentage:float=0.2, 
+                 warm_up_start_factor:float=0.01, 
+                 epochs:int=1) :
 
         self.model_config = model_config 
         self.model = model(self.model_config)
@@ -18,70 +28,91 @@ class TrainingLoop :
             weight_decay=weight_decay,
             betas=adamw_betas
         )
+        self.epochs = epochs
 
         print("getting training tokens and target tokens")
         self.training_batches, self.target_batches = self.data_loader.build_batches(data_set, batch_size)
         
-        self.training_batches = torch.tensor(self.training_batches).to(device=self.device, 
-                                                                        dtype=torch.int64)
+        self.training_batches = torch.from_numpy(self.training_batches)
         
-        self.target_batches = torch.tensor(self.target_batches).to(device=self.device, 
-                                                                   dtype=torch.int64)
+        self.target_batches = torch.from_numpy(self.target_batches)
+        
+        self.training_steps =  self.target_batches.shape[0] 
+        print(f"number of training iterations : {self.training_steps * self.epochs}")
 
-        self.training_steps =  self.target_batches.shape(0) 
-        print(f"number of training iterations : {self.training_steps}")
-
-        self.warmup_steps = warmup_step_percentage * self.training_steps
+        self.warmup_steps = int(warmup_step_percentage * self.training_steps)
         print(f"number of warmup iterations : {self.warmup_steps} ")
 
-        
+        self.decay_steps = int(decay_step_percentage * self.training_steps) 
+        print(f"number of decay steps : {self.decay_steps}")
+
+        self.stable_steps = self.training_steps - self.warmup_steps - self.decay_steps
+
 
 
         ##the learning rate start at 0.01 LR and start increasing until getting the peak which
-        warm_up_phase = torch.optim.LinearLR(
+        warm_up_phase = torch.optim.lr_scheduler.LinearLR(
             self.optimizer,
-            start_factor=0.01, 
-            total_iters=0.05*self.num_iter
+            start_factor=warm_up_start_factor,
+            end_factor=1.0 , 
+            total_iters=self.warmup_steps
         )
 
-        cosine_decay_phase = torch.optim.ConsineAnnealingLR(
+        stable_phase = torch.optim.lr_scheduler.ConstantLR(
             self.optimizer, 
-            T_max=self.training_steps - self.warmup_steps, 
-            eta_min=0.1*self.lr 
+            factor=1.0, 
+            total_iters=self.stable_steps
+        )
+
+        cosine_decay_phase = torch.optim.lr_scheduler.CosineAnnealingLR(
+            self.optimizer, 
+            T_max=self.decay_steps, 
+            eta_min=0.0
         )
 
         self.scheduler = torch.optim.lr_scheduler.SequentialLR(
             self.optimizer, 
-            schedulers=[warm_up_phase,consine_decay_phase] ,
-            milestones=[warm_up_phase]
+            schedulers=[warm_up_phase,stable_phase,cosine_decay_phase] ,
+            milestones=[self.warmup_steps, self.stable_steps+self.warmup_steps]
         )
 
-    def train() : 
+    def train(self) : 
         self.model.train() 
-        for i in self.batch_size :
-            self.optimizer.zero_grad() 
-            logits = self.model(self.training_batches[i])
-            loss = torch.nn.functional.cross_entropy(
-                logits.flatten(0,1) , 
-                self.target_batches[i].flatten()
-            )
-            loss.backward()
-            self.optimizer.step()
-            self.scheduler.step()
+        for epoch in range(self.epochs) : 
+            print(f"epoch : {epoch} ")
+            for i in range(self.training_steps) :
+                self.optimizer.zero_grad() 
+                logits = self.model(self.training_batches[i])
+                loss = torch.nn.functional.cross_entropy(
+                    logits.flatten(0,1) , 
+                    self.target_batches[i].flatten()
+                )
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(),max_norm=1.0)
+                self.optimizer.step()
+                self.scheduler.step()
 
+# def dff_for_target(N, V=32767, d=768, L=20, dkv=192, tied=True):
+#     fixed = (2 - tied) * V * d + L * (2*d*d + 2*d*dkv + 2*d) + d
+#     return (N - fixed) / (3 * L * d)
 
 if __name__ == "__main__" : 
 
     model_config = {
-        "d_model" : 576 , 
-        "num_heads" : 9, 
-        "num_kv_heads" : 3, 
-        "vocab_size" : 32767 , 
-        "n_layers" : 12, 
-        "context_window" : 2048
+        "d_model"        : 768,    
+        "num_heads"      : 16,
+        "num_kv_heads"   : 4,     
+        "d_ff"           : 1792,  
+        "vocab_size"     : 32767, 
+        "n_layers"       : 30,
+        "context_window" : 2048,  
     }
 
     llama_model = LlamaModel(model_config)
+    total = sum(p.numel() for p in llama_model.parameters())
+    trainable = sum(p.numel() for p in llama_model.parameters() if p.requires_grad)
+    print(f"total number of parameters : {total:,}") 
+    print(f"total number of trainable parameters :  {trainable:,}")
     data_loader = DataLoaderHF(tokenizer_file = "./fr_bpe_32k_422.json")
     npy_files = [
         "./training/training_tokens/manu_french_poetry_tokens.npy",
